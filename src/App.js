f4f7ae7e80c53cb8a080f398cf9341704b77c947
Denis Rybalka <denis.rubalka1@gmail.com>
2021-07-11 15:02:17 +0300

    recognizing & reading colors

--- src/App.js
+++ src/App.js
@@ -1,66 +1,84 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
+import Webcam from "react-webcam";
 
 const App = () => {
-  const [color, setColor] = useState("");
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const webcamRef = useRef(null);
   const canvasRef = useRef();
+  const [cube, setCube] = useState([]);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    updateCanvas(imageSrc);
+  }, [webcamRef]);
 
   useEffect(() => {
-    updateCanvas();
-  }, []);
+    setInterval(() => {
+      capture();
+    }, 1500);
+  }, [capture]);
 
-  const updateCanvas = () => {
+  const updateCanvas = (src) => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
-
     const img = new Image();
-    img.src =
-      "https://images.pexels.com/photos/7556079/pexels-photo-7556079.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260";
     img.crossOrigin = "Anonymous";
-
-    canvas.width = img.width;
-    canvas.height = img.height;
-
-    img.onload = function () {
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-
+    img.onload = () => {
+      canvas.height = img.naturalHeight;
+      canvas.width = img.naturalWidth;
+      ctx.drawImage(img, 0, 0);
       const img_data = ctx.getImageData(0, 0, img.width, img.height);
-      canvas.onmousemove = function (e) {
-        const x = e.offsetX;
-        const y = e.offsetY;
-        const pix = img_data.data;
+      defineColors(img_data, img);
+    };
+    img.src = src;
+  };
+
+  const defineColors = (img_data, img) => {
+    setCube([]);
+    const pix = img_data.data;
+
+    let x = 200,
+      y = 125;
+
+    for (let i = 0, id = 0; i < 3; i++, y += 100, x = 200) {
+      for (let j = 0; j < 3; j++, x += 100) {
         const i = (y * img.width + x) * 4;
         const R = pix[i];
         const G = pix[i + 1];
         const B = pix[i + 2];
         const rgb = `rgb(${R},${G},${B})`;
-        setColor(rgb);
-        setMousePosition({ x, y });
-      };
-    };
+
+        const block = {
+          x,
+          y,
+          color: rgb,
+          id,
+          R,
+          G,
+          B,
+        };
+
+        id++;
+        setCube((state) => [...state, block]);
+      }
+    }
   };
 
   return (
-    <div>
+    <div style={{ display: "flex" }}>
+      <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
       <canvas ref={canvasRef}></canvas>
-
-      <div
-        style={{
-          backgroundColor: color,
-          width: 30,
-          height: 30,
-          position: "absolute",
-          left: mousePosition.x + 20,
-          top: mousePosition.y + 20,
-          border: "2px dotted black",
-          borderRadius: "30px",
-        }}
-      ></div>
-      <span style={{position: "absolute",
-          left: mousePosition.x + 20,
-          top: mousePosition.y + 55}}>{color}</span>
+      <div className="grid">
+        {cube.map((el) => (
+          <div
+            key={el.id}
+            className="g"
+            style={{
+              backgroundColor: el.color,
+            }}
+          >{`X: ${el.x}, Y: ${el.y}, ID: ${el.id}, COLOR: ${el.color}`}</div>
+        ))}
+      </div>
     </div>
   );
 };
-
 export default App;
